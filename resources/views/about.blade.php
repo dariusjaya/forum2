a95027086551e11cd57b0ce6948f213d8a188c33
dariusjaya <117186725+dariusjaya@users.noreply.github.com>
2022-11-08 23:50:05 +0700

    Add files via upload

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('container')
+    <h1>About Us</h1>
+    <h3>{{ $name}}</h3>
+    <p>{{ $email}}</p>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('container')
+        <h1>Home</h1>
+        <h5>Info game terkini mengenai game favorit </h5>
+        <h6>-Dota 2</h6>
+        <img src="img/dota 2.jpg" alt="dota 2">
+        <h6>-Valorant</h6>
+        <img src="img/valorant.jpg" alt="valorant">
+
+@endsection
+
+
